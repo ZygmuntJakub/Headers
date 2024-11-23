ea337309a6471dd3f9dfeecbb2617f8b9cd81cb1
Jakub Zygmunt <jakub.zygmunt.afm@gmail.com>
2024-11-23 18:05:53 +0100

    migrate to manifest v3

--- background/index.js
+++ background/index.js
@@ -1,52 +1,77 @@
+const context =
+  typeof this.browser !== "undefined" ? this.browser : this.chrome;
+
 const syncIcon = (isActive) => {
   const path = isActive ? "../icons/icon-32-green.png" : "../icons/icon-32.png";
-  browser.browserAction.setIcon({ path });
+  context.action.setIcon({ path });
 };
 
-async function addHeadersFromStorage(e) {
-  return browser.storage.sync.get("isActive").then(({ isActive } = {}) => {
-    if (!isActive) {
-      return { requestHeaders: e.requestHeaders };
-    }
+const getId = (rules, index) =>
+  (rules.length ? rules[rules.length - 1].id : 0) + index + 1;
 
-    return browser.storage.sync
-      .get("headers")
-      .then(async ({ headers } = {}) => {
-        const { disabledHeaders = {} } =
-          await browser.storage.sync.get("disabledHeaders");
-
-        if (headers) {
-          for (const [key, value] of Object.entries(headers)) {
-            if (disabledHeaders[key]) {
-              continue;
+const syncRules = () => {
+  context.storage.sync.get("isActive", ({ isActive } = {}) => {
+    context.storage.sync.get("headers", ({ headers = {} } = {}) => {
+      context.storage.sync.get(
+        "disabledHeaders",
+        ({ disabledHeaders = {} } = {}) => {
+          context.declarativeNetRequest.getDynamicRules().then((rules) => {
+            if (!isActive) {
+              context.declarativeNetRequest.updateDynamicRules({
+                removeRuleIds: rules.map((rule) => rule.id),
+              });
+            } else {
+              context.declarativeNetRequest.updateDynamicRules({
+                removeRuleIds: rules
+                  .filter(
+                    (rule) =>
+                      disabledHeaders[rule.action.requestHeaders[0].header] ||
+                      !headers[rule.action.requestHeaders[0].header]
+                  )
+                  .map((rule) => rule.id),
+                addRules: Object.entries(headers)
+                  .filter(([key]) => !disabledHeaders[key])
+                  .filter(
+                    ([key, value]) =>
+                      !rules.some(
+                        (rule) =>
+                          rule.action.requestHeaders[0].header === key &&
+                          rule.action.requestHeaders[0].value === value
+                      )
+                  )
+                  .map(([key, value], index) => ({
+                    id: getId(rules, index),
+                    priority: 1,
+                    action: {
+                      type: "modifyHeaders",
+                      requestHeaders: [
+                        {
+                          operation: "set",
+                          header: key,
+                          value,
+                        },
+                      ],
+                    },
+                    condition: {
+                      urlFilter: "*",
+                      resourceTypes: Object.values(
+                        context.declarativeNetRequest.ResourceType
+                      ),
+                    },
+                  })),
+              });
             }
-            e.requestHeaders.push({ name: key, value });
-          }
+          });
         }
-
-        return { requestHeaders: e.requestHeaders };
-      });
+      );
+    });
   });
-}
-
-const run = (message) => {
-  if (message === "activate") {
-    syncIcon(true);
-    browser.webRequest.onBeforeSendHeaders.addListener(
-      addHeadersFromStorage,
-      { urls: ["<all_urls>"] },
-      ["blocking", "requestHeaders"],
-    );
-  } else if (message === "deactivate") {
-    syncIcon(false);
-    browser.webRequest.onBeforeSendHeaders.removeListener(
-      addHeadersFromStorage,
-    );
-  }
 };
 
-browser.runtime.onMessage.addListener(run);
-
-browser.storage.sync.get("isActive").then(({ isActive } = {}) => {
-  run(isActive ? "activate" : "deactivate");
+context.runtime.onMessage.addListener((message) => {
+  if (message === "syncRules") {
+    syncRules();
+  }
 });
+
+syncRules();
--- popup/index.js
+++ popup/index.js
@@ -1,9 +1,14 @@
+const context = chrome;
 const isActiveCheckbox = document.getElementById("isActiveCheckbox");
 
+const syncRules = () => {
+  context.runtime.sendMessage("syncRules");
+};
+
 const syncIsActive = () => {
-  browser.storage.sync.get("isActive").then((result) => {
+  context.storage.sync.get("isActive", (result) => {
     isActiveCheckbox.checked = result.isActive;
-    browser.browserAction.setIcon({
+    context.action.setIcon({
       path: result.isActive
         ? "../icons/icon-32-green.png"
         : "../icons/icon-32.png",
@@ -13,94 +18,102 @@ const syncIsActive = () => {
 };
 
 isActiveCheckbox.addEventListener("click", () => {
-  browser.storage.sync.get("isActive").then((result) => {
-    browser.storage.sync.set({ isActive: !result.isActive });
+  context.storage.sync.get("isActive", (result) => {
+    context.storage.sync.set({ isActive: !result.isActive });
     syncIsActive();
-    browser.runtime.sendMessage(result.isActive ? "deactivate" : "activate");
+    syncRules();
   });
 });
 
 const headersTableBody = document.getElementById("headersTableBody");
 
 const syncTable = () => {
-  browser.storage.sync.get("headers").then(async (result) => {
-    const { disabledHeaders = {} } =
-      await browser.storage.sync.get("disabledHeaders");
-    headersTableBody.innerHTML = "";
-    const headers = Object.entries(result.headers || {});
-    for (const [key, value] of headers) {
-      const row = document.createElement("tr");
-      const keyCell = document.createElement("td");
-      const keyInput = document.createElement("input");
-      const valueCell = document.createElement("td");
-      const valueInput = document.createElement("input");
-      const enableCheckbox = document.createElement("input");
-      const deleteButton = document.createElement("button");
+  context.storage.sync.get("headers", (result) => {
+    context.storage.sync.get(
+      "disabledHeaders",
+      ({ disabledHeaders = {} } = {}) => {
+        headersTableBody.innerHTML = "";
+        const headers = Object.entries(result.headers || {});
+        for (const [key, value] of headers) {
+          const row = document.createElement("tr");
+          const keyCell = document.createElement("td");
+          const keyInput = document.createElement("input");
+          const valueCell = document.createElement("td");
+          const valueInput = document.createElement("input");
+          const enableCheckbox = document.createElement("input");
+          const deleteButton = document.createElement("button");
 
-      enableCheckbox.checked = !disabledHeaders[key];
-      keyInput.value = key;
-      valueInput.value = value;
-      keyInput.type = "text";
-      valueInput.type = "text";
-      enableCheckbox.type = "checkbox";
+          enableCheckbox.checked = !disabledHeaders[key];
+          keyInput.value = key;
+          valueInput.value = value;
+          keyInput.type = "text";
+          valueInput.type = "text";
+          enableCheckbox.type = "checkbox";
 
-      keyInput.addEventListener("change", () => {
-        browser.storage.sync.get("headers").then((result) => {
-          const newHeaders = result.headers || {};
-          newHeaders[keyInput.value] = newHeaders[key];
-          delete newHeaders[key];
-          browser.storage.sync.set({ headers: newHeaders });
-          syncTable();
-        });
-      });
+          keyInput.addEventListener("change", () => {
+            context.storage.sync.get("headers", (result) => {
+              const newHeaders = result.headers || {};
+              newHeaders[keyInput.value] = newHeaders[key];
+              delete newHeaders[key];
+              context.storage.sync.set({ headers: newHeaders });
+              syncTable();
+              syncRules();
+            });
+          });
 
-      valueInput.addEventListener("change", () => {
-        browser.storage.sync.get("headers").then((result) => {
-          const newHeaders = result.headers || {};
-          newHeaders[keyInput.value] = valueInput.value;
-          browser.storage.sync.set({ headers: newHeaders });
-          syncTable();
-        });
-      });
+          valueInput.addEventListener("change", () => {
+            context.storage.sync.get("headers", (result) => {
+              const newHeaders = result.headers || {};
+              newHeaders[keyInput.value] = valueInput.value;
+              context.storage.sync.set({ headers: newHeaders });
+              syncTable();
+              syncRules();
+            });
+          });
 
-      enableCheckbox.addEventListener("click", () => {
-        browser.storage.sync.get("disabledHeaders").then((result) => {
-          const newDisabledHeaders = result.disabledHeaders || {};
-          newDisabledHeaders[key] = !enableCheckbox.checked;
-          browser.storage.sync.set({ disabledHeaders: newDisabledHeaders });
-          syncTable();
-        });
-      });
+          enableCheckbox.addEventListener("click", () => {
+            context.storage.sync.get("disabledHeaders").then((result) => {
+              const newDisabledHeaders = result.disabledHeaders || {};
+              newDisabledHeaders[key] = !enableCheckbox.checked;
+              context.storage.sync.set({ disabledHeaders: newDisabledHeaders });
+              syncTable();
+              syncRules();
+            });
+          });
 
-      deleteButton.addEventListener("click", () => {
-        browser.storage.sync.get("headers").then((result) => {
-          const newHeaders = result.headers || {};
-          delete newHeaders[key];
-          browser.storage.sync.set({ headers: newHeaders });
-        });
+          deleteButton.addEventListener("click", () => {
+            context.storage.sync.get("headers").then((result) => {
+              const newHeaders = result.headers || {};
+              delete newHeaders[key];
+              context.storage.sync.set({ headers: newHeaders });
+              syncRules();
+            });
 
-        browser.storage.sync.get("disabledHeaders").then((result) => {
-          const newDisabledHeaders = result.disabledHeaders || {};
-          delete newDisabledHeaders[key];
-          browser.storage.sync.set({ disabledHeaders: newDisabledHeaders });
-        });
-        row.remove();
-      });
+            context.storage.sync.get("disabledHeaders").then((result) => {
+              const newDisabledHeaders = result.disabledHeaders || {};
+              delete newDisabledHeaders[key];
+              context.storage.sync.set({ disabledHeaders: newDisabledHeaders });
+              syncRules();
+            });
+            row.remove();
+          });
 
-      keyCell.appendChild(keyInput);
-      valueCell.appendChild(valueInput);
-      row.style.opacity =
-        disabledHeaders[key] || !isActiveCheckbox.checked ? 0.5 : 1;
-      deleteButton.textContent = "Delete";
-      deleteButton.classList.add("deleteButton");
+          keyCell.appendChild(keyInput);
+          valueCell.appendChild(valueInput);
+          row.style.opacity =
+            disabledHeaders[key] || !isActiveCheckbox.checked ? 0.5 : 1;
+          deleteButton.textContent = "Delete";
+          deleteButton.classList.add("deleteButton");
 
-      row.appendChild(keyCell);
-      row.appendChild(valueCell);
-      row.appendChild(enableCheckbox);
-      row.appendChild(deleteButton);
+          row.appendChild(keyCell);
+          row.appendChild(valueCell);
+          row.appendChild(enableCheckbox);
+          row.appendChild(deleteButton);
 
-      headersTableBody.appendChild(row);
-    }
+          headersTableBody.appendChild(row);
+        }
+      }
+    );
   });
 };
 
@@ -118,13 +131,14 @@ const addHeader = () => {
     return;
   }
 
-  browser.storage.sync.get("headers").then((result) => {
+  context.storage.sync.get("headers", (result) => {
     const newHeaders = result.headers || {};
     newHeaders[values.key] = values.value;
-    browser.storage.sync.set({ headers: newHeaders });
+    context.storage.sync.set({ headers: newHeaders });
     headerKeyInput.value = "";
     headerValueInput.value = "";
     syncTable();
+    syncRules();
   });
 };
 
@@ -137,3 +151,4 @@ document.addEventListener("keypress", (e) => {
 
 syncTable();
 syncIsActive();
+syncRules();
